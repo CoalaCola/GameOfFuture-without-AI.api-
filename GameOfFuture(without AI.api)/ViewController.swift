1328ba5f5eb60097609f446d664e3cd673a72eb4
Vince <vince9458@yahoo.com.tw>
2017-08-02 18:28:26 +0800

    Future will let you know where your future is.

--- GameOfFuture(without AI.api)/ViewController.swift
+++ GameOfFuture(without AI.api)/ViewController.swift
@@ -7,19 +7,48 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var textView: UITextView!
+    
+    
+    let synth = AVSpeechSynthesizer()
+    var myUtterance = AVSpeechUtterance(string: "")
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
+    @IBAction func SendButton(_ sender: Any) {
+        var youSay = "\(textField.text)"
+        let oldTextView = textView.text
+        
+        
+        if youSay.contains("wealthy") ||
+           youSay.contains("money") {
+            textView.text = "Through little struggles in life you would become a person with all the things you have been dreaming of."
+        }
+            
+        else if youSay.contains("famous") ||
+           youSay.contains("celebrity") {
+            textView.text = "You need to be serous about this. Your work matters millions of people."
+        }
+        else if youSay.contains("love") ||
+           youSay.contains("married") ||
+           youSay.contains("boyfriend") ||
+           youSay.contains("girlfriend"){
+            textView.text = "Hold on, just try to love the people next to you ok?"
+        }
+        else {
+            textView.text = "I'm You from the future but I can not understand you. Try another question."
+        }
+        
+        myUtterance = AVSpeechUtterance(string: textView.text)
+        myUtterance.rate = 0.5
+        
+        if oldTextView != textView.text {
+        synth.speak(myUtterance)
+        }
+        
 }
 
+}
